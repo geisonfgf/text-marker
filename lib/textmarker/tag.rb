10d69b66e9cb7d88874b60e6398ac9a25ce68185
Geison Goes <geison.goes@hp.com>
2015-01-04 21:27:59 -0200

    Add validation to Tag class parameters and update test suite

--- lib/textmarker/tag.rb
+++ lib/textmarker/tag.rb
@@ -2,10 +2,15 @@ require "textmarker/version"
 
 module TextMarker
   class Tag
-    attr_accessor :tag, :background_color, :text_color
-    attr_reader :open_tag, :close_tag
+    attr_reader :tag, :open_tag, :close_tag, :background_color, :text_color
 
     def initialize(tag=:span, background_color=:yellow, text_color=:black)
+      @valid_tags = get_valid_html_tags
+
+      validate_tag(tag)
+      validate_color(background_color)
+      validate_color(text_color)
+
       @tag = tag
       @background_color = background_color
       @text_color = text_color
@@ -18,7 +23,47 @@ module TextMarker
       generate_open_tag + generate_close_tag
     end
 
+    def tag=(tag)
+      validate_tag(tag)
+      @tag = tag
+    end
+
+    def background_color=(background_color)
+      validate_color(background_color)
+      @background_color = background_color
+    end
+
+    def text_color=(text_color)
+      validate_color(text_color)
+      @text_color = text_color
+    end
+
+    protected
+
+      def get_valid_html_tags
+        Set.new [:h1, :h2, :h3, :h4, :h5, :h6, :span, :div, :p, :a, :button, :center, :strong, :u, :b, :i]
+      end
+
     private
+
+      def validate_tag(tag)
+        exception_error_message = ":tag must be one of these html tags "\
+                                 ":h1, :h2, :h3, :h4, :h5, :h6, :span, :div, "\
+                                 ":p, :a, :button, :center, :strong, :u, :b, :i"
+        raise(ArgumentError, exception_error_message) unless is_html_tag_valid? tag
+      end
+
+      def validate_color(color)
+        array_colors = [:aqua, :black, :blue, :fuchsia, :gray, :green, :lime, :maroon, :navy, :olive, :orange, :purple, :red, :silver, :teal, :white, :yellow]
+        exception_error_message = "The color must be a css color value like #ffffff or one of these standard colors "\
+                                 ":aqua, :black, :blue, :fuchsia, :gray, :green, :lime, :maroon, :navy, :olive, "\
+                                 ":orange, :purple, :red, :silver, :teal, :white, :yellow"
+        raise(ArgumentError, exception_error_message) if not array_colors.include? color and color.match(/#([a-fA-F0-9]){3}(([a-fA-F0-9]){3})?\b/).nil?
+      end
+
+      def is_html_tag_valid?(tag)
+        @valid_tags.include? tag
+      end
 
       def generate_open_tag
         "<#{@tag} style='background_color: #{@background_color}; color: #{@text_color};'>"
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
-describe "TextMarker::Tag.new" do
+describe "TextMarker::Tag" do
   before(:each) do
     @instance_default = TextMarker::Tag.new
     @instance = TextMarker::Tag.new :h1, :black, :white
+    @exception_error_message_for_tag = ":tag must be one of these html tags "\
+                                      ":h1, :h2, :h3, :h4, :h5, :h6, :span, :div, "\
+                                      ":p, :a, :button, :center, :strong, :u, :b, :i"
+    @exception_error_message_for_color = "The color must be a css color value like #ffffff or one of these standard colors "\
+                                        ":aqua, :black, :blue, :fuchsia, :gray, :green, :lime, :maroon, :navy, :olive, "\
+                                        ":orange, :purple, :red, :silver, :teal, :white, :yellow"
   end
 
   it 'Shoud create a Default Tag object' do
@@ -24,5 +30,53 @@ describe "TextMarker::Tag.new" do
     expect(@instance.background_color).to eq :black
     expect(@instance.text_color).to eq :white
     expect(@instance.to_s).to eq "<h1 style='background_color: black; color: white;'></h1>"
+  end
+
+  it 'Should raise a ArgumentException creating Tag' do
+    expect { TextMarker::Tag.new :error }.to raise_error(ArgumentError, @exception_error_message_for_tag)
+    expect { TextMarker::Tag.new :span, :error }.to raise_error(ArgumentError, @exception_error_message_for_color)
+    expect { TextMarker::Tag.new :span, :white, :error }.to raise_error(ArgumentError, @exception_error_message_for_color)
+  end
+
+  it 'Should set tag to :h3' do
+    expect(@instance_default.tag).to eq :span
+    @instance_default.tag = :h3
+    expect(@instance_default.tag).to eq :h3
+  end
+
+  it 'Should set background_color to :black' do
+    expect(@instance_default.background_color).to eq :yellow
+    @instance_default.background_color = :black
+    expect(@instance_default.background_color).to eq :black
+  end
+
+  it 'Should set background_color to #000000' do
+    expect(@instance_default.background_color).to eq :yellow
+    @instance_default.background_color = '#000000'
+    expect(@instance_default.background_color).to eq '#000000'
+  end
+
+  it 'Should set text_color to :white' do
+    expect(@instance_default.text_color).to eq :black
+    @instance_default.text_color = :white
+    expect(@instance_default.text_color).to eq :white
+  end
+
+  it 'Should set background_color to #ffffff' do
+    expect(@instance_default.text_color).to eq :black
+    @instance_default.text_color = '#ffffff'
+    expect(@instance_default.text_color).to eq '#ffffff'
+  end
+
+  it 'Should raise a ArgumentException setting Tag tag' do
+    expect { @instance_default.tag = :error }.to raise_error(ArgumentError, @exception_error_message_for_tag)
+  end
+
+  it 'Should raise a ArgumentException setting Tag background_color' do
+    expect { @instance_default.background_color = :error }.to raise_error(ArgumentError, @exception_error_message_for_color)
+  end
+
+  it 'Should raise a ArgumentException setting Tag text_color' do
+    expect { @instance_default.text_color = :error }.to raise_error(ArgumentError, @exception_error_message_for_color)
   end
 end
